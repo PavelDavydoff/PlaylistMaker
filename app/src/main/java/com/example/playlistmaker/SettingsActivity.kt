1c26b3ef07579f6202a71c0da497be251ce7453d
Pavel_Davydov <david20506@yandex.ru>
2023-11-15 18:10:46 +0500

    Создал ресурсы для тёмной темы

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<Button>(R.id.back)
+        val backButton = findViewById<ImageView>(R.id.back)
         backButton.setOnClickListener{
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
