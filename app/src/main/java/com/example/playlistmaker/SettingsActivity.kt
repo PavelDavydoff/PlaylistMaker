0b32944a5f2223687082d6da939a1638ec175179
Pavel_Davydov <david20506@yandex.ru>
2023-11-16 21:01:03 +0500

    Кнопки "Написать в поддержку" и "Пользовательское соглашение" теперь работают

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -15,6 +16,25 @@ class SettingsActivity : AppCompatActivity() {
         backButton.setOnClickListener{
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+
+        val supportButton = findViewById<Button>(R.id.support)
+        supportButton.setOnClickListener{
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("david20506@yandex.ru"))
+                putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
+                putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+            }
+
+            startActivity(supportIntent)
+        }
+
+        val agreementButton = findViewById<Button>(R.id.agreement)
+        agreementButton.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(browserIntent)
         }
     }
 }
