447967984f9d47dba1da68866668fb22fa9e7971
Pavel_Davydov <david20506@yandex.ru>
2023-11-18 20:21:28 +0500

    Вынес строки в ресурсы

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -15,7 +15,7 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<ImageView>(R.id.back)
+        val backButton = findViewById<ImageView>(R.id.back)//Кнопка "Назад"
         backButton.setOnClickListener{
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
@@ -23,10 +23,10 @@ class SettingsActivity : AppCompatActivity() {
 
 
 
-        val switchTheme = findViewById<Switch>(R.id.switchTheme)
+        val switchTheme = findViewById<Switch>(R.id.switchTheme)//Переключатель тёмной темы
 
         switchTheme.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
-        switchTheme.setOnCheckedChangeListener { buttonView, isChecked ->
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
@@ -34,36 +34,34 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
 
-        fun showShareDialog() {
-            val message = "https://practicum.yandex.ru/profile/android-developer/?from=catalog"
+        fun showShareDialog() {//Кнопка "Поделиться"
             val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, message)
-            startActivity(Intent.createChooser(intent, "Поделиться приложением"))
+            intent.type = resources.getString(R.string.text_plain)
+            intent.putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.android_developer))
+            startActivity(Intent.createChooser(intent, resources.getString(R.string.share)))
         }
 
         val shareButton = findViewById<Button>(R.id.share)
         shareButton.setOnClickListener {
-            // Handle button click event here
             showShareDialog()
         }
 
-        val supportButton = findViewById<Button>(R.id.support)
+        val supportButton = findViewById<Button>(R.id.support)//Кнопка "Написать в поддержку"
         supportButton.setOnClickListener{
             val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:")
-                putExtra(Intent.EXTRA_EMAIL, arrayOf("david20506@yandex.ru"))
-                putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
-                putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
+                data = Uri.parse(resources.getString(R.string.mail_to))
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.my_email)))
+                putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.dev_message))
+                putExtra(Intent.EXTRA_TEXT, resources.getString(R.string.dev_message2))
             }
 
             startActivity(supportIntent)
         }
 
-        val agreementButton = findViewById<Button>(R.id.agreement)
+        val agreementButton = findViewById<Button>(R.id.agreement)//Кнопка "Пользовательское соглашение"
         agreementButton.setOnClickListener {
             val browserIntent =
-                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+                Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.offer)))
             startActivity(browserIntent)
         }
     }
