3713f55b7ed651cf3750448242ba4839aa5751f2
Pavel_Davydov <david20506@yandex.ru>
2023-11-19 16:08:01 +0500

    Домашнее задание №3 9 спринта готово

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,6 +3,9 @@ package com.example.playlistmaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
@@ -15,5 +18,28 @@ class SearchActivity : AppCompatActivity() {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
         }
+
+        val editText = findViewById<EditText>(R.id.search_bar)
+        /*val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }*/
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+
+        editText.addTextChangedListener(simpleTextWatcher)
+
     }
 }
