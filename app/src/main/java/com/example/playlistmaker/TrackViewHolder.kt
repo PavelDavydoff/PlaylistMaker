9b859faf0807437bfe876c67d2440e8e46f358d2
Pavel_Davydov <david20506@yandex.ru>
2023-12-03 20:31:15 +0500

    Исправления в соответствии с замечаниями

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,7 +1,8 @@
 package com.example.playlistmaker
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,23 +10,25 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    //private var viewTrack = itemView.findViewById<FrameLayout>(R.id.view_track)
-    private var imageTrack = itemView.findViewById<ImageView>(R.id.image_track)
-    private var textTrackName = itemView.findViewById<TextView>(R.id.text_track_name)
-    private var textArtist = itemView.findViewById<TextView>(R.id.text_artist)
-    private var textTime = itemView.findViewById<TextView>(R.id.text_time)
-    //private var forwardArrow = itemView.findViewById<ImageView>(R.id.image_forward_arrow)
+    private val imageTrack = itemView.findViewById<ImageView>(R.id.image_track)
+    private val textTrackName = itemView.findViewById<TextView>(R.id.text_track_name)
+    private val textArtist = itemView.findViewById<TextView>(R.id.text_artist)
+    private val textTime = itemView.findViewById<TextView>(R.id.text_time)
 
     fun bind(model: Track){
         Glide.with(itemView)
             .load(model.artworkUrl100)
-            .transform(RoundedCorners(2))
             .centerCrop()
-            .placeholder(R.drawable.cross)
+            .placeholder(R.drawable.placeholder)
+            .transform(RoundedCorners(dpToPx(itemView, 2f)))
             .into(imageTrack)
         textTrackName.text = model.trackName
         textArtist.text = model.artistName
         textTime.text = model.trackTime
 
+    }
+    private fun dpToPx(view: View, dp: Float) : Int {
+        val displayMetrics = view.resources.displayMetrics
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).toInt()
     }
 }
