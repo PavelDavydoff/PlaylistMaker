6417493ddea31483feca6e2a5b391ad7a92ee70c
Pavel_Davydov <david20506@yandex.ru>
2024-03-05 22:15:11 +0500

    Добавлен MediaPlayer. Работает!

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,28 +1,34 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.databinding.ActivityPlayerBinding
 
-
 class PlayerActivity : AppCompatActivity() {
 
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
     private lateinit var binding: ActivityPlayerBinding
+    private var url: String? = null
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var playButton: ImageView
+    private var playerState = STATE_DEFAULT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
         val track = intent.getSerializableExtra(SearchActivity.INTENT_KEY) as Track
 
         val backButton = findViewById<ImageView>(R.id.backButton)
@@ -37,6 +43,7 @@ class PlayerActivity : AppCompatActivity() {
             .transform(RoundedCorners(this.resources.getDimensionPixelSize(R.dimen.corner_2)))
             .into(binding.artwork)
 
+        url = track.previewUrl
         binding.trackName.text = track.trackName
         binding.artistName.text = track.artistName
         binding.collectionNameV.text = track.collectionName
@@ -44,6 +51,67 @@ class PlayerActivity : AppCompatActivity() {
         binding.yearV.text = track.releaseDate.substring(0, 4)
         binding.genreV.text = track.primaryGenreName
         binding.countryV.text = track.country
+
+        playButton = findViewById(R.id.playButton)
+
+        if (url != null) {
+            preparePlayer()
+            playButton.setOnClickListener {
+                playbackControl()
+            }
+        } else {
+            playButton.setOnClickListener {
+                Toast.makeText(applicationContext, "Url = null", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
     }
 
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setImageResource(R.drawable.play_button)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setImageResource(R.drawable.pause_button)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setImageResource(R.drawable.play_button)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
 }
